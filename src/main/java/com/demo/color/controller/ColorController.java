3027e1c6016fe54b44fca2c3aa2ad12180a1b970
Seah <code.angz@gmail.com>
2023-03-06 01:12:25 +0900

    0306 db에 파일첨부 해야함, 당연함

---
+++ src/main/java/com/demo/color/controller/ColorController.java
@@ -0,0 +1,63 @@
+package com.demo.color.controller;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+public class ColorController {
+
+	@GetMapping("/colormain")
+	public void colormain() {
+		log.info("======색조 추천 탭======");
+	}
+
+	@GetMapping("/colorAtMouse")
+	public String getColorAtMouse() throws AWTException {
+		PointerInfo pointerInfo = MouseInfo.getPointerInfo();
+		Point point = pointerInfo.getLocation();
+		Color color = new Robot().getPixelColor((int) point.getX(), (int) point.getY());
+		int red = color.getRed();
+		int green = color.getGreen();
+		int blue = color.getBlue();
+		return String.format("RGB: (%d, %d, %d)", red, green, blue);
+	}
+	
+	@GetMapping("/color2")
+    public String getImage(Model model) {
+        String imageUrl = "/img/IMG_0184.JPG"; // Replace with the actual URL of your image
+        BufferedImage image = getImgPixel(imageUrl);
+
+        if (image != null) {
+            model.addAttribute("image", image);
+            model.addAttribute("pixel", new Color(image.getRGB(image.getWidth()-1, image.getHeight()-1)));
+        }
+
+        return "color2";
+    }
+
+    private BufferedImage getImgPixel(String url) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+}
