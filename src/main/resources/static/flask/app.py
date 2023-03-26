bfe22638cf66a2c5970702a4273c8de86c937b3a
Seah <code.angz@gmail.com>
2023-03-26 22:26:31 +0900

    conflict

--- src/main/resources/static/flask/app.py
+++ src/main/resources/static/flask/app.py
@@ -22,8 +22,7 @@ def s3_connection():
         s3 = boto3.client(
             service_name="s3",
             region_name="ap-northeast-2",
-            aws_access_key_id=${{ secrets.AWS_ACCESS_KEY_ID }},
-            aws_secret_access_key=${{ secrets.AWS_SECRET_ACCESS_KEY },
+            #aws secret key 복사붙여넣기 해라
         )
     except Exception as e:
         print(e)
